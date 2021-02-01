7f4a34f837a6851e68b0587abdce30b7570ff46b
Matt Macarty <matt.macarty@gmail.com>
2021-02-01 15:41:06 -0500

    class files

---
+++ program_files/reaction_tester.py
@@ -0,0 +1,27 @@
+import random
+from time import time, sleep
+
+total_time = 0
+average_time = 0
+print("Welcome to thje reaction time tester")
+print("When the prompt apprears hit enter!")
+sentinel = 1
+for run in range(3):
+
+    print("Get ready")
+    prompt = random.random() * 5
+    sleep(prompt)
+    start_time = time()
+    print("Quick, hit enter!!")
+    input()
+    reaction_time = time() - start_time
+    total_time += reaction_time
+    print(f"Wow, that was fast. It took {reaction_time:.3f}")
+    #print(sentinal)
+    average_time = total_time / sentinel
+    sentinal += 1
+    print("-" * 25)
+    print(f"Average time was {average_time:.3f}")
+
+
+
